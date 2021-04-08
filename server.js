11766649287dc382e6f0e56b4b1c2e002d58ebf8
msmalinosterberg <msmalinosterberg@gmail.com>
2021-04-08 11:19:08 +0200

    post endpoint done with random id

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000; 
 
 //Lägger till en middleware
-//All inkommande anrop ska body parsas från json till JS 
+//All inkommande anrop ska body parsas från json till JavaScript 
 app.use(express.json())
 
 const courses = [
@@ -37,10 +37,9 @@ const courses = [
     }, 
 ]; 
 
-//Rootsidan 
-app.get('/', (req, res) => {
-    res.json('hello world') //route handler 
-}); 
+//Serve alla filer i public filen
+app.use(express.static('./public'))
+
 
 //Visar alla kurser 
 app.get('/api/courses', (req, res) => {
@@ -50,23 +49,29 @@ app.get('/api/courses', (req, res) => {
 
 //Lägger till en kurs 
 app.post('/api/courses', (req, res) => {
-    const course = {
-        id: courses.length +1, 
-        name: req.body.name
-    }; 
+    const newCourse = {
+        id: Date.now(),
+        name: req.body.name, 
+        points: req.body.points, 
+        location: req.body.location
+    }
     res.status(201); // Requesten var successful 
-    courses.push(course); 
-    res.json(course); //skicka alltid tillbaka json
+    courses.push(newCourse); 
+    res.json(courses); //skicka alltid tillbaka json
 })
 
 
 //Hämtar en specifik kurs med ett id 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id)); 
-  if(!course) res.status(404).send('The course with the given ID was not found')
+  if(!course) return res.status(404).send('The course with the given ID was not found')
   res.json(course); 
 })
 
+
+app.delete('api/courses', (req, res) => {
+    
+})
 
 //Startar servern 
 app.listen(port, () => console.log(` Server is running at http://localhost:${port}`)); 
